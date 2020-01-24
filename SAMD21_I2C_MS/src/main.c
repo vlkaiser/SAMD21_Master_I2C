3eb5a2e9d83a7b657b75e374e1e3fbe606e8630a
Vicki <vlkaiser9@gmail.com>
2020-01-24 11:48:07 -0500

    Updated: BAUD state, Added: BAUD calculations.  Clock speed changes with SPEED and BAUD params

--- SAMD21_I2C_MS/src/main.c
+++ SAMD21_I2C_MS/src/main.c
@@ -26,25 +26,32 @@
 ***************************************************************************************************************************/
 #include <asf.h>
 
-#define STANDARD_MODE_FAST_MODE	0x0		// I2C Speed Mode Standard
-#define FAST_MODE_PLUS	0x01			// I2C SPEED bit field
-#define HIGHSPEED_MODE	0x02			// I2C SPEED bit field
-#define SLAVE_ADDR	0x12				// SLAVE device Address
+#define STANDARD_MODE_FAST_MODE	0x0				// I2C Speed Mode Standard
+#define FAST_MODE_PLUS			0x01			// I2C SPEED bit field
+#define HIGHSPEED_MODE			0x02			// I2C SPEED bit field
+#define SLAVE_ADDR				0x12			// SLAVE device Address
 
-#define BUF_SIZE	3					// TX/RX Buffer Size
+#define F_GCLK			48000000			//48MHz
+#define F_SCL_STD		100000				//100khz	- w/ STD or FastMode+ = 20khz
+#define F_SCL_FAST		400000				//400khz - w/ STD or FastMode+ = 76khz
+#define F_SCL_FASTPLUS	1000000				//1Mhz - w/ STANDARD_MODE_FAST_MODE = 243khz
+#define F_SCL_HS		3400000				//3.4MHz
+
+#define BUF_SIZE		3					// TX/RX Buffer Size
 
 
 /* GLOBALS */
 uint8_t i;
 volatile bool tx_done = false, rx_done = false;
 uint8_t tx_buf[BUF_SIZE] = {1, 2, 3};
-uint8_t rx_buf[BUF_SIZE];
+uint8_t rx_buf[BUF_SIZE];
 
 /* Prototypes */
 void i2c_clock_init(void);
 static void pin_set_peripheral_function(uint32_t pinmux);
 void i2c_pin_init(void);
 uint32_t calculate_baud(uint32_t fgclk, uint32_t fscl);
+uint32_t calculate_baud_high(uint32_t fgclk, uint32_t fscl);
 void i2c_master_init(void);
 void i2c_master_transaction(void);
 
@@ -73,7 +80,7 @@ void i2c_clock_init()
  * @param[in]			- pinmux (MCU driver files for pin definitions)
  * @return				- void
  *
- * @note				- Assign I/O lines PA08 and PA09 to the SERCOM peripheral function. *						- Will switch the GPIO functionality of an I/O pin to peripheral
+ * @note				- Assign I/O lines PA08 and PA09 to the SERCOM peripheral function. *						- Will switch the GPIO functionality of an I/O pin to peripheral
  *							 functionality and assigns the given peripheral function to the pin.
  ******************************************************************************************************/
 static void pin_set_peripheral_function(uint32_t pinmux)
@@ -102,15 +109,36 @@ void i2c_pin_init()
 /******************************************************************************************************
  * @fn					- calculate_baud
  * @brief				- Calculate the BAUD value using f_{gclk},f_{scl}, t_{rise}
+ *						  BAUDLOW = 0
  * @param[in]			- fgclk
  *						- fscl
  *
  * @return				- f_baud
  *
  * @note				- FSCL = fGCLK / (10 + BAUD +BAUDLOW + fGCLKTRISE )
- *						
+ *						  eg param: fgclk (@48MHz), fscl (@100khz, @1MHz...)
  ******************************************************************************************************/
 uint32_t calculate_baud(uint32_t fgclk, uint32_t fscl)
+{
+	float f_temp, f_baud;
+	f_baud = (((float)fgclk/(float)fscl) - 10 - ((float)fgclk*0.0000003))/2;
+
+	return ((uint32_t)f_baud);
+}
+
+/******************************************************************************************************
+ * @fn					- calculate_baud_low
+ * @brief				- Calculate the BAUDLOW value using f_{gclk},f_{scl}, t_{rise}
+ *						  Given BAUDLOW (TLOW) is approx 2x BAUD (THIGH)
+ * @param[in]			- fgclk
+ *						- fscl
+ *
+ * @return				- f_baud
+ *
+ * @note				- FSCL = fGCLK / (10 + BAUD +BAUDLOW + fGCLKTRISE )
+ *						  eg param: fgclk (@48MHz -> 4800), fscl (@100khz -> 100, @1MHz -> 1000)
+ ******************************************************************************************************/
+uint32_t calculate_baud_high(uint32_t fgclk, uint32_t fscl)
 {
 	float f_temp, f_baud;
 	f_temp = ((float)fgclk/(float)fscl) - (((float)fgclk/(float)1000000)*0.3);
@@ -130,14 +158,61 @@ uint32_t calculate_baud(uint32_t fgclk, uint32_t fscl)
  *						
  ******************************************************************************************************/
 void i2c_master_init()
-{	/* Configurations while I2C is DISABLED: */	/* Configure SERCOM_I2CM hardware register CTRLA:	*	- SPEED bit field as 0x01, I2C Master runs at Fast mode + - 1MHz
+{
+	/* Configurations while I2C is DISABLED: */
+
+	/* Configure SERCOM_I2CM hardware register CTRLA:
+	*	- SPEED bit field as 0x01, I2C Master runs at Fast mode + - 1MHz
 	*	- SDAHOLD bit field as 0x02, SDA hold time is configured for 300-600ns
 	*	- RUNSTDBY bit as 0x01, Generic clock is enabled in all sleep modes (any interrupt can wake up the device)
-	*	- MODE bitfield to 0x5, SERCOM2 is configured as I2C Master	*/	SERCOM2->I2CM.CTRLA.reg =	SERCOM_I2CM_CTRLA_SPEED(0x01)			|								SERCOM_I2CM_CTRLA_SDAHOLD(0x2)			|								SERCOM_I2CM_CTRLA_RUNSTDBY				|								SERCOM_I2CM_CTRLA_SCLSM					|								SERCOM_I2CS_CTRLA_MODE_I2C_MASTER;	/* Enable Smart Mode - Will ACK when DATA.DATA is read*/	SERCOM2->I2CM.CTRLB.reg = SERCOM_I2CM_CTRLB_SMEN;	/* Synchronization Busy - Writing CTRLB.CMD or CTRLB.FIFOCLR, STATUS.BUSSTATE, ADDR, or DATA when the SERCOM is
-	enabled requires synchronization. When written, the SYNCBUSY.SYSOP bit will be set until
-	synchronization is complete.*/	while(SERCOM2->I2CM.SYNCBUSY.bit.SYSOP);	/* BAUDLOW sets SCL low time, BAUD sets SCL high time 	   fSCL = 1MHz, fGCLK = 48MHz (default), trise = 100ns.	   Using datasheet calc, BAUD + BAUDLOW = 33 (tlow =~ 2x thigh) */	//SERCOM2->I2CM.BAUD.reg = SERCOM_I2CM_BAUD_BAUD(11) | SERCOM_I2CM_BAUD_BAUDLOW(22);	/* Wait for Sync */	while(SERCOM2->I2CM.SYNCBUSY.bit.SYSOP);	/* Enabled SERCOM2 Peripheral */	SERCOM2->I2CM.CTRLA.reg |= SERCOM_I2CM_CTRLA_ENABLE;	/* SERCOM Enable synchronization busy (Wait) */
-	while((SERCOM2->I2CM.SYNCBUSY.reg & SERCOM_I2CM_SYNCBUSY_ENABLE));	/* BusState to Idle (Forced) eg when in unknown state*/	SERCOM2->I2CM.STATUS.bit.BUSSTATE = 0x1;	/* Wait for Sync */	while(SERCOM2->I2CM.SYNCBUSY.bit.SYSOP);	/* Enable Interrupt: Master on bus, Slave on Bus [INTterrupt ENable SET 	   Enable Receive Ready Interrupt Master position, slave position pg 610*/	SERCOM2->I2CM.INTENSET.reg = SERCOM_I2CM_INTENSET_MB | SERCOM_I2CM_INTENSET_SB;	/* Enable SERCOM2 interrupt handler */
-	system_interrupt_enable(SERCOM2_IRQn);}/******************************************************************************************************
+	*	- MODE bitfield to 0x5, SERCOM2 is configured as I2C Master
+	*/
+	SERCOM2->I2CM.CTRLA.reg = SERCOM_I2CM_CTRLA_SPEED(STANDARD_MODE_FAST_MODE)	|
+								SERCOM_I2CM_CTRLA_SDAHOLD(0x2)			|
+								SERCOM_I2CM_CTRLA_RUNSTDBY				|
+								//SERCOM_I2CM_CTRLA_SCLSM					|
+								SERCOM_I2CS_CTRLA_MODE_I2C_MASTER;
+
+	/* Enable Smart Mode - Will ACK when DATA.DATA is read*/
+	SERCOM2->I2CM.CTRLB.reg = SERCOM_I2CM_CTRLB_SMEN;
+
+	/* Synchronization Busy - Writing CTRLB.CMD or CTRLB.FIFOCLR, STATUS.BUSSTATE, ADDR, or DATA when the SERCOM is
+	enabled requires synchronization. When written, the SYNCBUSY.SYSOP bit will be set until synchronization is complete.*/
+	while(SERCOM2->I2CM.SYNCBUSY.bit.SYSOP);
+
+	/* BAUDLOW sets SCL low time, BAUD sets SCL high time 
+	   fSCL = 1MHz, fGCLK = 48MHz (default), trise = 100ns.
+	   Using datasheet calc, BAUD + BAUDLOW = 33 (tlow =~ 2x thigh) 
+	   BAUDLOW = 0, BAUD.BAUD sets SCL High and SCL Low, trise (std) = 3us?
+	   */
+	//SERCOM2->I2CM.BAUD.reg = SERCOM_I2CM_BAUD_BAUD(11) | SERCOM_I2CM_BAUD_BAUDLOW(22);
+	SERCOM2->I2CM.BAUD.reg = SERCOM_I2CM_BAUD_BAUD(calculate_baud(F_GCLK, F_SCL_FAST)) | SERCOM_I2CM_BAUD_BAUDLOW(0);
+
+	/* Wait for Sync */
+	while(SERCOM2->I2CM.SYNCBUSY.bit.SYSOP);
+
+	/* Enabled SERCOM2 Peripheral */
+	SERCOM2->I2CM.CTRLA.reg |= SERCOM_I2CM_CTRLA_ENABLE;
+
+	/* SERCOM Enable synchronization busy (Wait) */
+	while((SERCOM2->I2CM.SYNCBUSY.reg & SERCOM_I2CM_SYNCBUSY_ENABLE));
+
+	/* BusState to Idle (Forced) eg when in unknown state*/
+	SERCOM2->I2CM.STATUS.bit.BUSSTATE = 0x1;
+
+	/* Wait for Sync */
+	while(SERCOM2->I2CM.SYNCBUSY.bit.SYSOP);
+
+	/* Enable Interrupt: Master on bus, Slave on Bus [INTterrupt ENable SET 
+	   Enable Receive Ready Interrupt Master position, slave position pg 610*/
+	SERCOM2->I2CM.INTENSET.reg = SERCOM_I2CM_INTENSET_MB | SERCOM_I2CM_INTENSET_SB;
+
+	/* Enable SERCOM2 interrupt handler */
+	system_interrupt_enable(SERCOM2_IRQn);
+
+}
+
+/******************************************************************************************************
  * @fn					- i2c_master_transaction
  * @brief				- Perform a transaction with the connected slave device
  * @param[in]			- void
@@ -153,7 +228,7 @@ void i2c_master_transaction(void)
 	/* Acknowledge behavior: 0 = send ACK in ACKACT bit CTRLB */
 	SERCOM2->I2CM.CTRLB.reg &= ~SERCOM_I2CM_CTRLB_ACKACT;
 
-	/* Wait for Sync */	while(SERCOM2->I2CM.SYNCBUSY.bit.SYSOP);
+	/* Wait for Sync */	while(SERCOM2->I2CM.SYNCBUSY.bit.SYSOP);
 
 	/* load I2C Slave Address into reg, and Write(0) in 0th bit to Slave.  Initiate Transfer */
 	SERCOM2->I2CM.ADDR.reg = (SLAVE_ADDR << 1) | 0;
@@ -163,7 +238,7 @@ void i2c_master_transaction(void)
 	/* ACK is sent */
 	SERCOM2->I2CM.CTRLB.reg &= ~SERCOM_I2CM_CTRLB_ACKACT;
 
-	/* Wait for Sync */	while(SERCOM2->I2CM.SYNCBUSY.bit.SYSOP);
+	/* Wait for Sync */	while(SERCOM2->I2CM.SYNCBUSY.bit.SYSOP);
 
 	/* Read (1) in 0th bit, from Slave (ACK) */
 	SERCOM2->I2CM.ADDR.reg = (SLAVE_ADDR << 1) | 1;
@@ -193,7 +268,7 @@ void i2c_master_transaction(void)
 			/* After transferring the last byte, send stop condition */
 			SERCOM2->I2CM.CTRLB.bit.CMD = 0x3;
 		
-			/* Wait for Sync */			while(SERCOM2->I2CM.SYNCBUSY.bit.SYSOP);
+			/* Wait for Sync */			while(SERCOM2->I2CM.SYNCBUSY.bit.SYSOP);
 
 			tx_done = true;
 			i = 0;
@@ -211,7 +286,7 @@ void i2c_master_transaction(void)
 		{
 				/* NACK Should be sent BEFORE reading the last byte */
 				SERCOM2->I2CM.CTRLB.reg |= SERCOM_I2CM_CTRLB_ACKACT;
-							/* Wait for Sync */				while(SERCOM2->I2CM.SYNCBUSY.bit.SYSOP);
+							/* Wait for Sync */				while(SERCOM2->I2CM.SYNCBUSY.bit.SYSOP);
 			
 				/* Send stop condition */
 				SERCOM2->I2CM.CTRLB.bit.CMD = 0x3;
@@ -273,10 +348,6 @@ int main (void)
 		if (port_pin_get_input_level(BUTTON_0_PIN) == BUTTON_0_ACTIVE) {
 			/* Yes, so turn LED on. */
 			port_pin_set_output_level(LED_0_PIN, LED_0_ACTIVE);
-			
-			/* and run i2c transaction */
-			i2c_master_transaction();
-
 		} else {
 			/* No, so turn LED off. */
 			port_pin_set_output_level(LED_0_PIN, !LED_0_ACTIVE);
